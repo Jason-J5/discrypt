9684bea56a36759eee43c703d957c6c5941263ad
Jason-J5 <jacobsjason421@gmail.com>
2022-02-21 16:49:05 -0500

    initial push

---
+++ pgp.plugin.js
@@ -0,0 +1,23 @@
+function getName(){
+    return 'Discrypt';
+}
+
+function getDescription(){
+    return "adds pgp to discord"
+}
+
+function getVersion(){
+    return "0.1.1"    
+}
+
+function getAuthor(){
+    return "Purple"
+}
+
+function start(){
+
+}
+
+function stop(){
+    
+}
