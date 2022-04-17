a70250948e179b8ba1ef3fdb73412b70a4ac455e
Jason-J5 <jacobsjason421@gmail.com>
2022-04-17 14:01:10 -0400

    pgp testing

---
+++ pgp.js
@@ -0,0 +1,14 @@
+const openpgp = require('openpgp'); // use as CommonJS, AMD, ES6 module or via window.openpgp
+
+(async () => {
+    const { privateKey, publicKey } = await openpgp.generateKey({
+        type: 'rsa', // Type of the key
+        rsaBits: 4096, // RSA key size (defaults to 4096 bits)
+        userIDs: [{ name: 'Jon Smith', email: 'jon@example.com' }], // you can pass multiple user IDs
+        passphrase: 'super long and hard to guess secret' // protects the private key
+    });
+
+    console.log(publicKey)
+    console.log(privateKey)
+})();
+
