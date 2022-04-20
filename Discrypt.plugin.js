0ce6f4e8637cbd31e5948958faa8d40b1513e680
Jason-J5 <jacobsjason421@gmail.com>
2022-04-20 15:22:11 -0400

    broken stuff

--- Discrypt.plugin.js
+++ Discrypt.plugin.js
@@ -5,18 +5,49 @@
  * @version 0.0.1
  * @donate 
  * @source https://github.com/Jason-J5/discrypt
- * @website 
+ * @website http://Jason-J5.github.io/discrypt
  */
 
+  // use as CommonJS, AMD, ES6 module or via window.openpgp
+
+  var pubKey = require('./pgp.js');
+  console.log(pubKey)
+
+
  module.exports = class Discrypt {
-    load() {} // Optional function. Called when the plugin is loaded in to memory
+    load() {
+        //
+    } // Optional function. Called when the plugin is loaded in to memory
 
     start() {
+        //const openpgp = require('openpgp');
         BdApi.showToast("Discrypt plugin loaded!", {type:"success", icon: true});
-    } // Required function. Called when the plugin is activated (including after reloads)
+        
+        // async function yesy() { //set the code to repeat without pausing discord itself.
+        //     const { privateKey, publicKey } = await openpgp.generateKey({
+        //         type: 'rsa', // Type of the key
+        //         rsaBits: 4096, // RSA key size (defaults to 4096 bits)
+        //         userIDs: [{ name: 'Jon Smith', email: 'jon@example.com' }], // you can pass multiple user IDs
+        //         passphrase: 'super long and hard to guess secret' // protects the private key
+        //     });
+        //     BdApi.showToast(publicKey);
+        //  };
+         //yesy();
+         console.log("test")
+        }
+        // console.log(publicKey)
+        // console.log(privateKey)
+        // return publicKey;
+
+
+     // Required function. Called when the plugin is activated (including after reloads)
     stop() {
         BdApi.showToast("Discrypt plugin stopped!", {type:"success", icon: true});
     } // Required function. Called when the plugin is deactivated
 
     observer(changes) {} // Optional function. Observer for the `document`. Better documentation than I can provide is found here: <https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver>
+
+    
+    
 }
+
--- pgp.js
+++ pgp.js
@@ -1,14 +1,38 @@
+module.export function genKey(){
+
 const openpgp = require('openpgp'); // use as CommonJS, AMD, ES6 module or via window.openpgp
 
-(async () => {
+async function getKeys(){
     const { privateKey, publicKey } = await openpgp.generateKey({
         type: 'rsa', // Type of the key
         rsaBits: 4096, // RSA key size (defaults to 4096 bits)
         userIDs: [{ name: 'Jon Smith', email: 'jon@example.com' }], // you can pass multiple user IDs
         passphrase: 'super long and hard to guess secret' // protects the private key
     });
+    return [publicKey, privateKey]
+   // console.log(privateKey)
+}
+}
 
-    console.log(publicKey)
-    console.log(privateKey)
-})();
+(async () => {
+    pubkey = (await getKeys())[0];
+    //console.log(pubkey);
+    
+  })()
 
+
+
+
+
+// const { subtle } = require('crypto').webcrypto;
+
+//         async function generateAesKey(length = 256) {
+//           const key = await subtle.generateKey({
+//             name: 'AES-CBC',
+//             length
+//           }, true, ['encrypt', 'decrypt']);
+//           console.log(key);
+//           return key;
+//         }
+
+//         console.log(generateAesKey())
